b032cd536a13b0d7988c86be7a44f99f85d6aabf
cmalenfant <malenfantc@hotmail.fr>
2016-11-23 09:04:16 -0500

    prog finale

--- TP3_retroaction.ino
+++ TP3_retroaction.ino
@@ -1,15 +1,22 @@
-/* Utilisation du capteur Ultrason HC-SR04 */
 #include <Servo.h>
+#include <NewPing.h>
+
+#define MAX_DISTANCE 200
+#define TRIGGER_PIN 12
+#define ECHO_PIN 11
 
 Servo servo;
 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+
 // définition des broches utilisées
-int angle = 0;
+int angle = 150;
 int trig = 12;
 int echo = 11;
 long lecture_echo;
 long cm;
-int threshold = 8;
+int threshold = 4;
 bool descenteMannequin = true;
 
 void setup()
@@ -19,33 +26,27 @@ void setup()
   digitalWrite(trig, LOW);
   pinMode(echo, INPUT);
   servo.attach(9);
+  servo.write(angle);
 }
 
 void loop()
 {
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  lecture_echo = pulseIn(echo, HIGH);
-  cm = lecture_echo / 58;
-  Serial.println(cm);
+  unsigned int uS = sonar.ping_cm();
+  Serial.println(uS);
 
-  if(cm <= threshold && descenteMannequin == true) {
-    descenteMannequin = false;
-    remonterMannequin();
+  if(descenteMannequin == true) {
+      if(uS <= threshold) {
+        descenteMannequin = false;
+      }
+    servo.write(angle -= 1);
   }
   
-  if (descenteMannequin == false && angle == 0) {
-    descenteMannequin = true;
-    descendreMannequin();
+  if (descenteMannequin == false) {
+     if(angle >= 150) {
+      descenteMannequin = true;
+     }
+    servo.write(angle += 1);
   }
-}
-
-void remonterMannequin () {
-  servo.write(angle = angle -1);
-}
-
-void descendreMannequin () {
-  servo.write(angle = angle + 1);
+  delay(20);
 }
 
